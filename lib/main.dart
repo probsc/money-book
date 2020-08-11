8399d21330a35bf98d4ea89104302790477f55d6
araseki kento <araseki_kento@nanairo-inc.jp>
2020-08-11 11:49:54 +0900

    #1 [お小遣い帳] AppBar にタブを実装

--- lib/main.dart
+++ lib/main.dart
@@ -4,114 +4,112 @@ void main() {
   runApp(MyApp());
 }
 
+/// # MyApp クラス
+/// 
+/// ## アプリ本体
+/// アプリのメイン画面を表示
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
+/// # メイン画面ウィジェット
+/// 
+/// ## お小遣い帳のメイン画面
+/// タブ、項目一覧、月表示を表示
+/// 
+/// ```
+/// home: MyHomePage()
+/// ```
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  const MyHomePage({ Key key }) : super(key: key);
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+class _MyHomePageState extends State<MyHomePage> with  SingleTickerProviderStateMixin {
+    final List<Tab> tabs = <Tab>[
+    Tab(text: '一覧'),
+    Tab(text: '月表示'),
+  ];
+  
+  TabController _tabController;
+  
+  int _index = 0;
+  
+   @override
+   void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: tabs.length);
+   }
+  
+   @override
+   void dispose() {
+    super.dispose();
+    _tabController.dispose();
+   }
+  
+   @override
+   Widget build(BuildContext context) {
     return Scaffold(
+      // AppBar にタブを配置
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        flexibleSpace: Padding(
+          padding: EdgeInsets.only(top: 27),
+          child: Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+           Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.blue)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  // 一覧/月表示ボタンを配置
+                  FlatButton(
+                    color: _index == 0 ? Colors.white : Colors.blue,
+                    onPressed: () {
+                      _tabController.animateTo(0);
+                      setState(() {
+                        _index = 0;
+                      });
+                    },
+                    child: Text(tabs[0].text),
+                  ),
+                  FlatButton(
+                    color: _index == 1 ? Colors.white : Colors.blue,
+                    onPressed: () {
+                      _tabController.animateTo(1);
+                      setState(() {
+                        _index = 1;
+                      });
+                    },
+                    child: Text(tabs[1].text),
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+          ]
+         )
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: TabBarView(
+        controller: _tabController,
+        // タブ以外で画面をスクロールさせないように設定
+        physics: const NeverScrollableScrollPhysics(),
+        children: tabs.map((tab) {
+          return Center(
+            child: Text('${tab.text}'),
+          );
+        }).toList(),
+      )
     );
   }
 }
