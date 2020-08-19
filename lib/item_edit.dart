3659dab023d9227e631bf485ba573781d7ef1c29
araseki kento <araseki_kento@nanairo-inc.jp>
2020-08-19 13:47:55 +0900

    #4 [お小遣い帳] 入力項目のレイアウトを調整

--- lib/item_edit.dart
+++ lib/item_edit.dart
@@ -39,6 +39,8 @@ class ItemEditState extends State<ItemEdit> {
     return Scaffold(
       // キーボード表示時に、項目入力画面がせり上がらないように設定
       resizeToAvoidBottomInset: false,
+      // 画面の背景色を設定
+      backgroundColor: Color(0xFFD9FCFF),
       appBar: AppBar(
         actions: <Widget>[
           // 編集時のみ削除ボタンを表示
@@ -62,84 +64,106 @@ class ItemEditState extends State<ItemEdit> {
       body: Column(
         children: <Widget>[
           // 日付入力
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text('日付'),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      // 選択した日付をダイアログに表示
-                      setState(() {
-                        _selectDate(context).then(
-                          (date) => _dateController.text = date != null
-                              ? DateFormat('yyyy-MM-dd').format(date)
-                              : '',
-                        );
-                      });
-                    },
-                    // TextField タップ時にキーボードを表示しないように AbsorbPointer でラップ
-                    child: AbsorbPointer(
-                      child: TextField(
-                        controller: _dateController,
-                        maxLines: 1,
-                        decoration:
-                            const InputDecoration(hintText: '日付を入力してください'),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+            child: Container(
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text('日付'),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          // 選択した日付をダイアログに表示
+                          setState(() {
+                            _selectDate(context).then(
+                              (date) => _dateController.text = date != null
+                                  ? DateFormat('yyyy-MM-dd').format(date)
+                                  : '',
+                            );
+                          });
+                        },
+                        // TextField タップ時にキーボードを表示しないように AbsorbPointer でラップ
+                        child: AbsorbPointer(
+                          child: TextField(
+                            controller: _dateController,
+                            maxLines: 1,
+                            decoration:
+                                const InputDecoration(hintText: '日付を入力してください'),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
+
           // 項目名入力
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text('項目'),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: TextField(
-                    // 項目入力
-                    controller: _nameController,
-                    maxLines: 1,
-                    decoration: const InputDecoration(hintText: '項目名を入力してください'),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+            child: Container(
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text('項目'),
                   ),
-                ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: TextField(
+                        // 項目入力
+                        controller: _nameController,
+                        maxLines: 1,
+                        decoration:
+                            const InputDecoration(hintText: '項目名を入力してください'),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
+
           // 金額入力
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text('金額'),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: TextField(
-                    // 金額入力
-                    controller: _priceController,
-                    keyboardType: TextInputType.number, // 入力キーボードを数字のみに制限
-                    maxLines: 1,
-                    // 入力は数字のみ受け付ける
-                    inputFormatters: <TextInputFormatter>[
-                      WhitelistingTextInputFormatter.digitsOnly,
-                    ],
-                    decoration: const InputDecoration(hintText: '金額を入力してください'),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+            child: Container(
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text('金額'),
                   ),
-                ),
-              )
-            ],
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: TextField(
+                        // 金額入力
+                        controller: _priceController,
+                        keyboardType: TextInputType.number, // 入力キーボードを数字のみに制限
+                        maxLines: 1,
+                        // 入力は数字のみ受け付ける
+                        inputFormatters: <TextInputFormatter>[
+                          WhitelistingTextInputFormatter.digitsOnly,
+                        ],
+                        decoration:
+                            const InputDecoration(hintText: '金額を入力してください'),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
           // ジャンル入力
           Container(
